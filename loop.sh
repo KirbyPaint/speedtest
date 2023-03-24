c351b7f373a9109d48413e021db54a37ee599f36
Ash Porter <porter.ashley13@gmail.com>
2023-03-23 18:39:15 -0700

    save

--- loop.sh
+++ loop.sh
@@ -6,7 +6,7 @@ do
   # echo "Processing $f file..."
   # take action on each file. $f store current file name
   # jq '.download,.upload,.ping,.timestamp' $f >> ./speedtest.txt
-  jq 'keys'  '{download: .download, upload: .upload, ping: .ping, timestamp: .timestamp}' "$f" >> ./testing.txt
+  jq 'keys' '.download' "$f" >> ./testing.txt
   jq  '{download: .download, upload: .upload, ping: .ping, timestamp: .timestamp}' "$f" >> ./testing.txt
   jq  '{download: .download, upload: .upload, ping: .ping, timestamp: .timestamp}' "$f" >> ./testing.txt
   jq  '{download: .download, upload: .upload, ping: .ping, timestamp: .timestamp}' "$f" >> ./testing.txt
