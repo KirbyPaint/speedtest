09ee38fd30ae074a2d57d027d96bce20da0b8e4d
Ash Porter <porter.ashley13@gmail.com>
2023-03-25 14:56:32 -0700

    cleanup, convention

--- loop.sh
+++ loop.sh
@@ -2,14 +2,19 @@
 
 set -euo pipefail
 
-FILES="./json/*.json"
+files="./json/*.json"
+headers=false
 
 # Blank out the file so no duplicate data
-true > ./testing.txt
+true > ./speedtest.csv
 
-for f in $FILES
+for f in $files
 do
-  cat < "$f" | jq -r 'keys[] as $k | "\($k): \(.[$k])"' >> ./testing.txt
+  if [ "$headers" = false ]; then
+    # Get wanted keys (don't need any of the nested keys in my case)
+    cat < "$f" | jq -r 'del(.server,.client,.share) | keys_unsorted | join(",")' >> ./speedtest.csv
+    headers=true
+  fi
+  # Get values for each key and turn into a comma-separated list
+  cat < "$f" | jq -r 'del(.server,.client) | . | join(",")' >> ./speedtest.csv
 done
-
-echo "all done :)"
